46265c318a9b1ce93247bf93ceede775cab33fb2
Chris Stook <chris@chrisstook.com>
2015-07-28 02:20:09 -0400

    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- test/test1.jl
+++ test/test1.jl
@@ -1,7 +1,8 @@
 filename = "test1.asc"
 exc = defaultLTspiceExcutable()
 test1 = LTspiceSimulation(exc,filename)
+LTspice.readlog!(test1)
 
-@test test1["Vin"] = 5
-@test test1["load"] = 2
-@test test1["Current"] = 2.5
+@test test1["Vin"] == 5
+@test test1["load"] == 2
+@test test1["current"] == 2.5

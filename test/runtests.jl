02ceaa0c0fa1400b48db38c2b48199f15b26dcf5
Chris Stook <chris@chrisstook.com>
2015-07-29 00:14:04 -0400

    added test2
    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using LTspice
 using Base.Test
 
 include("test1.jl")
+include("test2.jl")
 
 @test 1 == 1
---
+++ test/test2.jl
@@ -0,0 +1,15 @@
+filename = "test2.asc"
+exc = defaultLTspiceExcutable()
+test2 = LTspiceSimulation(exc,filename)
+LTspice.readlog!(test2)
+
+@test_approx_eq(test2["c"],2.0)
+@test_approx_eq(test2["b"],8.0)
+@test_approx_eq(test2["a"],10.0)
+@test_approx_eq(test2["d"],100.0)
+@test_approx_eq(test2["x"],1.00394)
+@test_approx_eq(test2["z"],0.019685)
+@test_approx_eq(test2["y"],0.984252)
+
+@test(length(test2.meas)==3)
+@test(length(test2.param)==4)

35e2f5018583cdbd04b9e60e136d84156a3fae87
Chris Stook <chris@chrisstook.com>
2015-08-19 12:30:42 -0400

    started CircuitFile type
    Signed-off-by: Chris Stook <chris@chrisstook.com>

---
+++ src/CircuitFile.jl
@@ -0,0 +1,138 @@
+# overloard parse for the CircuitFile type
+# used to parse LTspice circuit files *.asc
+import Base: parse, show
+
+type CircuitFile
+	circuitpath			:: ASCIIString
+	circuitfilearray 	:: Array{ASCIIString,1}    # text of circuit file
+	parameters 			:: Dict{ASCIIString,Tuple{Float64,Float64,Int}} # dictionay of parameters (value, multiplier, index)
+  	measurementnames    :: Array{ASCIIString,1}              # measurment names
+  	sweeps				:: Dict{ASCIIString,Int}   # dictionary of number of points in each sweep
+end
+
+
+function show(io::IO, x::CircuitFile)
+	println(io,x.circuitpath)
+  	println(io,"")
+  	println(io,"Parameters")
+  	for (key,(value,m,i)) in x.parameters
+    	println(io,"$(rpad(key,25,' ')) = $value")
+  	end
+ 	if length(x.measurementnames)>0 
+ 		println(io,"")
+ 		println(io,"Measurments")
+ 	end
+ 	for m in x.measurementnames
+ 		println(io,m)
+ 	end
+ 	if length(x.sweeps)>0
+ 		println(io,"")
+ 		println(io,"Sweeps")
+ 	end
+ 	println(io,"")
+ 	for (s,p) in x.sweeps
+ 		println(io,"$(rpad(s,25,' '))  $p steps")
+ 	end
+end
+ 	
+# units as defined in LTspice
+units = Dict()
+units["K"] = 1.0e3
+units["k"] = 1.0e3
+units["MEG"] = 1.0e6
+units["meg"] = 1.0e6
+units["G"] = 1.0e9
+units["g"] = 1.0e9
+units["T"] = 1.0e12
+units["t"] = 1.0e12
+units["M"] = 1.0e-3
+units["m"] = 1.0e-3
+units["U"] = 1.0e-6
+units["u"] = 1.0e-6
+units["N"] = 1.0e-9
+units["n"] = 1.0e-9
+units["P"] = 1.0e-12
+units["p"] = 1.0e-12
+units["F"] = 1.0e-15
+units["f"] = 1.0e-15
+
+
+function parse(::Type{CircuitFile}, circuitpath::ASCIIString)
+  # reads circuit file and returns a tuple of
+  # Dict of parameters
+  # Dict of measurements, values N/A
+  # circuit file array
+  #     The circuit file array is an array of strings which when concatenated produce the circuit file
+  #     The elements of the array split the file around parameter values to avoid parsing the file
+  #     every time a parameter is modified
+
+  ltspicefile = readall(circuitpath)            # read the circuit file
+
+  # create empty dictionarys to be filled as file is parsed
+  parameters = Dict{ASCIIString,Tuple{Float64,Float64,Int}}()     # Dict of parameters.  key = parameter, value = (parameter value, multiplier, circuit file array index)
+  measurementnames = Array(ASCIIString,0)
+  sweeps	= Dict{ASCIIString,Int}()
+  circuitfilearray = Array(ASCIIString,1)
+  circuitfilearray[1] = ""
+  # regex used to parse file.  I know this is a bad comment.
+  match_tags = r"(TEXT .*?(!|;)|
+                 .(param|PARAM)[ ]+([A-Za-z0-9]*)[= ]*([0-9.eE+-]*)(.*?)(?:\r|\\n|$)|
+                 .(measure|MEASURE|meas|MEAS)[ ]+(?:ac|AC|dc|DC|op|OP|tran|TRAN|tf|TF|noise|NOISE)[ ]+(\S+)[ ]+|
+                 .(step|STEP))"mx
+
+  # parse the file
+  directive = false   # true for directives, false for comments
+  m = match(match_tags,ltspicefile)
+  i = 1  # index for circuit file array
+  position = 1   # pointer into ltspicefile
+  old_position = 1
+  while m!=nothing
+    commentordirective = m.captures[2]    # ";" starts a comment, "!" starts a directive
+    isparamater = m.captures[3]!=nothing  # true for parameter card
+    parametername = m.captures[4]
+    parametervalue = m.captures[5]
+    parameterunit = m.captures[6]
+    ismeasure = m.captures[7]!=nothing   # true for measurement card
+    measurementname = m.captures[8]
+    isstep = m.captures[9]!=nothing
+    # determine if we are processign a comment or directive
+    if commentordirective == "!"
+      directive = true
+    elseif commentordirective == ";"
+      directive = false
+    end
+    if directive
+      if isparamater  # this is a paramater card
+        if haskey(units,parameterunit) # if their is an SI unit
+          multiplier = units[parameterunit] # find the multiplier
+        else
+          multiplier = 1.0 # if no unit, multiplier is 1.0
+        end
+        valuenounit = try  # try to convert the value.  might just want to let the exception happen...
+          parse(Float64,parametervalue)
+        catch
+          convert(Float64,NaN)
+        end
+        old_position = position
+        position = m.offsets[5]   # offset of the begining if the value in the circuit file
+        circuitfilearray = vcat(circuitfilearray,ltspicefile[old_position:position-1])  # text before the value
+        i += 1
+        circuitfilearray = vcat(circuitfilearray,ltspicefile[position:position+length(parametervalue)-1])  # text of the value
+        i += 1
+        parameters[parametername] = (valuenounit * multiplier, multiplier, i)
+        position = position+length(parametervalue)
+      end
+      if ismeasure  # this is a measurement card
+        key = lowercase(measurementname)  # measurements are all lower case in log file
+        push!(measurementnames,key)
+      end
+      if isstep # this is a step card
+        error(".step directive not supported")
+      end
+    end
+    m = match(match_tags,ltspicefile,m.offset+length(m.match))   # find next match
+  end
+  circuitfilearray = vcat(circuitfilearray,ltspicefile[position:end])  # the rest of the circuit
+  return CircuitFile(circuitpath, circuitfilearray, parameters, measurementnames, sweeps)
+end
+

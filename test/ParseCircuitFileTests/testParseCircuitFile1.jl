6bf3fb3fc872a0380699c781a59f3d325dbb7f23
Chris Stook <chris@chrisstook.com>
2015-08-24 18:41:15 -0400

    added first test for stepped simulation
    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- test/ParseCircuitFileTests/testParseCircuitFile1.jl
+++ test/ParseCircuitFileTests/testParseCircuitFile1.jl
@@ -1,4 +1,20 @@
 using LTspice
+using Base.Test
 
+PCF_test1file = "ParseCircuitFileTests/PCF_test1.asc"
+exc = ""
+PCF_test1 = LTspiceSimulation!(PCF_test1file,exc)
 
-
+alist = [1.0,2.0]
+blist = [10.0,15.0,20.0,25.0]
+clist = [100.0,200.0,300.0]
+verify = zeros(Float64,2,2,4,3)
+for (i,a) in enumerate(alist)
+    for (j,b) in enumerate(blist)
+        for (k,c) in enumerate(clist)
+            verify[1,i,j,k] = a+b+c
+            verify[2,i,j,k] = a+b+c+1000
+        end
+    end
+end
+@test getmeasurements(PCF_test1) == verify

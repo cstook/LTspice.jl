17aa7659251398760ca4fd78ca9b74cb3fc28a1a
Chris Stook <chris@chrisstook.com>
2015-08-24 18:53:15 -0400

    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,6 @@ using Base.Test
 include("test1.jl")
 include("test2.jl")
 include("test3.jl")
-include("ParseCircuitFileTests/testParseCircuitFile1.jl")
+#include("ParseCircuitFileTests/testParseCircuitFile1.jl")
 
 @test 1 == 1

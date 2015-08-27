4d9c52058aa76776d9f17162cdba6d23b13645dd
Chris Stook <chris@chrisstook.com>
2015-08-27 16:48:09 -0400

    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- test/test1.jl
+++ test/test1.jl
@@ -2,18 +2,22 @@ filename = "test1.asc"
 exc = ""  # null string will not run LTspice.exe.  Test parsing only.
 test1 = LTspiceSimulation!(filename,exc)
 show(test1)
+show(test1.circuit)
+show(test1.log)
 @test test1["Vin"] == 5
 @test test1["load"] == 2
 @test test1["current"] == 2.5
 show(test1)
-#@test getsteps(test1) == (Float64[],Float64[],Float64[])
+show(test1.circuit)
+show(test1.log)
 @test getmeasurementnames(test1) == ["current"]
-#@test getstepnames(test1) == []
 @test getlogpath(test1) != ""
 @test getcircuitpath(test1) != ""
 @test typeof(getparameters(test1)) == Dict{ASCIIString,Float64}
 @test getmeasurements(test1)[1,1,1,1] == 2.5
 @test getltspiceexecutablepath(test1) == ""
+@test eltype(test1.circuit) == Type(Float64)
+
 
 t = try
   LTspiceSimulation(filename)

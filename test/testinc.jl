77935ab214dc60fabe1ec6fc3b3a8a665a7bd924
Chris Stook <chris@chrisstook.com>
2016-10-23 22:31:05 -0400

    Signed-off-by: Chris Stook <chris@chrisstook.com>

--- test/testinc.jl
+++ test/testinc.jl
@@ -1,5 +1,5 @@
 function testinc1()
-  sim = LTspiceSimulation("testInc1.asc",tempdir=true)
+  sim = LTspiceSimulation("testInc1.asc",executablepath="",tempdir=true)
   show(IOBuffer(),sim)
   @test measurementnames(sim) == ("incA","incB","incC","incD","incE","incF")
 end
